fac4bf5367048d7ec8d6006c666ded580f7577de
Shawn <lightpnr@gmail.com>
2013-08-28 14:28:38 +0800

    Add example for CCBAnimationManager.

--- example/ccbi.lua
+++ example/ccbi.lua
@@ -1,10 +1,20 @@
 -- Create a autorelease CCBProxy.
-local p = LuaProxy:create()
+local proxy = LuaProxy:create()
 -- Read a layer from ccbi file.
-local n = p:readCCBFromFile"ALayer.ccbi"
+local n = proxy:readCCBFromFile"ALayer.ccbi"
 -- Pick up a node named "Label", cast to label, set the string.
-tolua.cast(p:getNode"Label", "CCLabelTTF"):setString"This is a label."
+tolua.cast(proxy:getNode"Label", "CCLabelTTF"):setString"This is a label."
 -- Create a scene and run
 local s = CCScene:create()
 s:addChild(tolua.cast(n, "CCLayer"))
 CCDirector:sharedDirector():runWithScene(s)
+
+-- Play another timeline (sequence)
+local am = CCBAnimationManager:pickFromNode(n) -- "n" is returned from LuaProxy:readCCBFromFile()
+am:runAnimations"2nd Timeline"
+-- Handle timeline complate event.
+-- "proxy" must be retained.
+-- Callback: CCBAnimationManager *am, string timelineName, LuaEventHandler *hnd
+proxy:handleAnimationComplate(am, function(am, timelineName, hnd)
+	-- do something after timeline complate
+end)
